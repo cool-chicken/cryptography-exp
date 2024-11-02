23c10eb8ca6383da2491147f20d9b0874fc9a988
cool-chicken <986595317@qq.com>
2024-11-02 17:42:54 +0800

    experiment3-f

--- 密码学实验三/challen39.py
+++ 密码学实验三/challen39.py
@@ -1,43 +1,53 @@
-def gcd(a,b):
-    if b ==0:
-        return a
-    else: 
-        return gcd(b,a%b)
-    
-def invmod(a,b):
-    for i in range(b):
-        if (a*i)%b == 1:
-            return i
-e = 3     
-def RSA_e(p,q,m):
-    n = p*q
-    et = (p-1)*(q-1)
-    if gcd(e,et) != 1:
-        print("e与et不互质")
-        return
-    c = pow(m,e,n)
-    print("加密后的信息为:{}\n公钥[e,n]:[{},{}]".format(c,e,n))
+from Crypto.Util.number import getPrime
 
-def RSA_d(p,q,c):
-    n = p*q
+def invmod(a, b):
+    def egcd(a, b):
+        if a == 0:
+            return b, 0, 1
+        g, x1, y1 = egcd(b % a, a)
+        x = y1 - (b // a) * x1
+        y = x1
+        return g, x, y
+
+    g, x, y = egcd(a, b)
+    if g != 1:
+        raise ValueError('模逆不存在')
+    else:
+        return x % b
+
+def generate_rsa_keys(bits=1024):
+    p = getPrime(bits)
+    q = getPrime(bits)
+    e = 3  
+    n = p * q
     et = (p-1)*(q-1)
+    if et % e == 0:
+        raise ValueError("e和et不互素，需要重新生成p和q")
     d = invmod(e,et)
-    if d is None:
-        print("没有找到模逆")
-        return
-    m = pow(c,d,n)
-    print("加密后的信息为:{}\n私钥[d,n]:[{},{}]".format(m,d,n))
+    return (e,n),(d,n)
 
-if __name__ == "__main__":
-    RSA_e(29,17,42)
-    RSA_d(29,17,138)
-# 加密后的信息为:138
-# 公钥[e,n]:[3,493]
-# 加密后的信息为:42
-# 私钥[d,n]:[299,493]
-    RSA_e(1019,2027,42)
-    RSA_d(1019,2027,74088)
-# 加密后的信息为:74088
-# 公钥[e,n]:[3,2065513]
-# 加密后的信息为:42
-# 私钥[d,n]:[1374979,2065513]
+def RSA_e(m,public_key):
+    e,n = public_key
+    c = pow(m,e,n)
+    return c
+
+def RSA_d(c,private_key):
+    d,n = private_key
+    m = pow(c,d,n)
+    return m
+
+def main():
+    public_key, private_key = generate_rsa_keys(bits=1024)
+    print("Public Key:", public_key)
+    print("Private Key:", private_key)
+    #m = 42
+    message = "Happy Holloween!"
+    m = int.from_bytes(message.encode('utf-8'), byteorder='big')
+    c = RSA_e(m,public_key)
+    print("Encrypted message:",c)
+    m = RSA_d(c,private_key)
+    message = m.to_bytes((m.bit_length() + 7) // 8, byteorder='big').decode('utf-8')
+    print("Decrypted message:",message)
+    # print("Decrypted message:",m)
+
+main()
